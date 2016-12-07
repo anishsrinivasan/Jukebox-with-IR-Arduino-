59c503a65887e7d3f3a29002d3ca1127952ff94e
Anish Srinivasan <itsanishsrinivasan@gmail.com>
2016-12-08 04:19:32 +0530

    Rename jukebox.ino.ino to jukebox.ino

---
+++ jukebox.ino
@@ -0,0 +1,165 @@
+/* YourDuino.com Example Software Sketch
+ IR Remote Kit Test
+ Uses YourDuino.com IR Infrared Remote Control Kit 2
+ http://arduino-direct.com/sunshop/index.php?l=product_detail&p=153
+ based on code by Ken Shirriff - http://arcfn.com
+ Get Library at: https://github.com/shirriff/Arduino-IRremote
+ Unzip folder into Libraries. RENAME folder IRremote
+ terry@yourduino.com */
+
+/*-----( Import needed libraries )-----*/
+
+#include "IRremote.h"
+int receiver = 9;
+#include <LiquidCrystal.h>
+char ch;
+int Contrast=15;
+// initialize the library with the numbers of the interface pins
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+/*-----( Declare Constants )-----*/
+ // pin 1 of IR receiver to Arduino digital pin 11
+
+/*-----( Declare objects )-----*/
+IRrecv irrecv(receiver);           // create instance of 'irrecv'
+decode_results results;            // create instance of 'decode_results'
+/*-----( Declare Variables )-----*/
+
+
+void setup()   /*----( SETUP: RUNS ONCE )----*/
+{
+  analogWrite(6,Contrast);
+  // set up the LCD's number of columns and rows: 
+  lcd.begin(16, 2);
+  lcd.print("Juke BOX");
+  analogWrite(9,28836);
+  Serial.begin(9600);
+  Serial.println("IR Receiver Raw Data + Button Decode Test");
+  irrecv.enableIRIn(); // Start the receiver
+
+}/*--(end setup )---*/
+
+
+void loop()   /*----( LOOP: RUNS CONSTANTLY )----*/
+{
+  if (irrecv.decode(&results)) // have we received an IR signal?
+
+  {
+//    Serial.println(results.value, HEX);  UN Comment to see raw values
+    translateIR(); 
+    irrecv.resume(); // receive the next value
+  }  
+}/* --(end main loop )-- */
+
+/*-----( Declare User-written Functions )-----*/
+void translateIR() // takes action based on IR code received
+
+// describing Car MP3 IR codes 
+
+{
+
+  switch(results.value)
+
+  {
+
+  case 0x1FE48B:  
+  lcd.setCursor(0,1);
+      lcd.print("Power ON        "); 
+    break;
+
+  case 0x1FEE01F:  
+    lcd.setCursor(0,1);
+      lcd.print("Waaazzzzzzzzuup?");
+Serial.write(1);
+    break;
+
+  case 0x1FE50AF:  
+    lcd.setCursor(0,1);
+      lcd.print("How you doin?   ");
+      Serial.write(2);
+    break;
+
+  case 0x1FED827:  
+  lcd.setCursor(0,1);
+      lcd.print("Knock Knock     ");
+      Serial.write(3);
+    break;
+
+  case 0x1FEF807:  
+    lcd.setCursor(0,1);
+      lcd.print("Challenge Accepted");
+      Serial.write(4);
+    break;
+
+  case 0xFFE01F:  
+    Serial.println(" VOL-           "); 
+    break;
+
+  case 0xFFA857:  
+    Serial.println(" VOL+           "); 
+    break;
+
+  case 0xFF906F:  
+    Serial.println(" EQ             "); 
+    break;
+
+  case 0xFF6897:  
+    Serial.println(" 0              "); 
+    break;
+
+  case 0xFF9867:  
+    Serial.println(" 100+           "); 
+    break;
+
+  case 0xFFB04F:  
+    Serial.println(" 200+           "); 
+    break;
+
+  case 0xFF30CF:  
+    Serial.println(" 1              "); 
+    break;
+
+  case 0xFF18E7:  
+    Serial.println(" 2              "); 
+    break;
+
+  case 0xFF7A85:  
+    Serial.println(" 3              "); 
+    break;
+
+  case 0xFF10EF:  
+    Serial.println(" 4              "); 
+    break;
+
+  case 0xFF38C7:  
+    Serial.println(" 5              "); 
+    break;
+
+  case 0xFF5AA5:  
+    Serial.println(" 6              "); 
+    break;
+
+  case 0xFF42BD:  
+    Serial.println(" 7              "); 
+    break;
+
+  case 0xFF4AB5:  
+    Serial.println(" 8              "); 
+    break;
+
+  case 0xFF52AD:  
+    Serial.println(" 9              "); 
+    break;
+
+  default: 
+    Serial.println(" other button   ");
+
+  }
+
+  delay(500);
+
+
+} //END translateIR
+
+
+
+/* ( THE END ) */
